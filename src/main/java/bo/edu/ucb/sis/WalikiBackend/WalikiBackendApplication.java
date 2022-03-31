f947d1391a37f8238c61bae39ae3a7e3ef6f2d87
Serius69 <seriogionis@gmail.com>
2022-03-31 08:01:42 -0400

    backend v0.1

---
+++ src/main/java/bo/edu/ucb/sis/WalikiBackend/WalikiBackendApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.sis.WalikiBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalikiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalikiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/sis/WalikiBackend/WalikiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.sis.WalikiBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalikiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
