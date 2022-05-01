9c0fe130242a26ddbc4fa53e72a7d2bf27b7ff3c
Serius69 <seriogionis@gmail.com>
2022-05-01 19:37:16 -0400

    [DEV] Keycloak

--- src/main/java/bo/ucb/edu/walikispring/dao/CountryRepository.java
+++ src/main/java/bo/ucb/edu/walikispring/dao/CountryRepository.java
@@ -4,7 +4,9 @@ import bo.ucb.edu.walikispring.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/foo")
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
 public interface CountryRepository extends JpaRepository<Country, Integer> {
